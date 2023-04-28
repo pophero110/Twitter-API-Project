0cc210a181dd84670cc6d5f9379c1eac277db38c
Jeff Ou <ojn2014520@gmail.com>
2023-04-28 13:06:39 -0500

    Use exception in TweetService

--- src/main/java/com/jeffdev/twitterapi/service/TweetService.java
+++ src/main/java/com/jeffdev/twitterapi/service/TweetService.java
@@ -1,5 +1,6 @@
 package com.jeffdev.twitterapi.service;
 
+import com.jeffdev.twitterapi.exception.InformationInvalidException;
 import com.jeffdev.twitterapi.model.Tweet;
 import com.jeffdev.twitterapi.repository.TweetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,20 +22,17 @@ public class TweetService {
      * @return The newly saved tweet
      */
     public Tweet createTweet(Tweet tweetObject) {
-        Tweet tweet = new Tweet();
         try {
             if (tweetObject.getContent().isBlank()) {
-                tweet.setContent("Content can not be empty or contains only space character");
+                throw new InformationInvalidException("Content can not be empty or contains only space character");
             }
             else if (tweetObject.getContent().length() > 280) {
-                tweet.setContent("Content can not be longer than 280 character");
+                throw new InformationInvalidException("Content can not be longer than 280 character");
             } else {
                 return tweetRepository.save(tweetObject);
             }
-            return tweet;
         } catch (Exception e) {
-            tweet.setContent(e.getMessage());
-            return tweet;
+            throw new InformationInvalidException(e.getMessage());
         }
     }
 }

87808bc9f81d5b1f3463f371da489263e98dedb4
Jeff Ou <ojn2014520@gmail.com>
2023-04-28 11:35:38 -0500

    Create tweet controller
    1. add a post request
    2. replace the 'spring-boot-starter' package with the 'spring-boot-starter-web' package of version 3.0.5.

---
+++ src/main/java/com/jeffdev/twitterapi/controller/TweetController.java
@@ -0,0 +1,24 @@
+package com.jeffdev.twitterapi.controller;
+
+
+import com.jeffdev.twitterapi.model.Tweet;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/twitters")
+public class TweetController {
+
+
+    /**
+     * Create a new tweet and return it
+     * @param tweetObject The tweet object to be created
+     * @return The newly created tweet object
+     */
+    @PostMapping(path = "")
+    public Tweet createTweet(@RequestBody Tweet tweetObject) {
+        return new Tweet();
+    }
+}

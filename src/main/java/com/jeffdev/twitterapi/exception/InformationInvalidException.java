b98fd0d504b7bf54989dfa5c6d9b9b744833b547
Jeff Ou <ojn2014520@gmail.com>
2023-04-28 12:58:54 -0500

    Replace InformationBlankException with InformationInvalidException

--- src/main/java/com/jeffdev/twitterapi/exception/InformationInvalidException.java
+++ src/main/java/com/jeffdev/twitterapi/exception/InformationInvalidException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class InformationBlankException extends RuntimeException {
-    public InformationBlankException(String message) {
+public class InformationInvalidException extends RuntimeException {
+    public InformationInvalidException(String message) {
         super(message);
     }
 }

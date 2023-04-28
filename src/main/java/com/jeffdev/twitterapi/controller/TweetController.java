29fa3d15353a4259ce392a9d2ab77419d3f393a1
Jeff Ou <ojn2014520@gmail.com>
2023-04-28 15:24:16 -0500

    Create DELETE endpoint for deleting a tweet

--- src/main/java/com/jeffdev/twitterapi/controller/TweetController.java
+++ src/main/java/com/jeffdev/twitterapi/controller/TweetController.java
@@ -37,12 +37,24 @@ public class TweetController {
 
     /**
      * update a specific tweet based on tweet id and return updated tweet
-     * @param tweetId the id of the tweet
+     *
+     * @param tweetId     the id of the tweet
      * @param tweetObject the object that contains attributes that is needed to be updated
      * @return updated tweet
      */
     @PutMapping(path = "/{tweetId}")
     public Tweet updateTweet(@PathVariable Long tweetId, @RequestBody Tweet tweetObject) {
         return tweetService.updateTweet(tweetId, tweetObject);
+    }
+
+    /**
+     * delete a specific tweet based on tweet id and return deleted tweet
+     *
+     * @param tweetId the id of the tweet
+     * @return deleted tweet
+     */
+    @DeleteMapping(path = "/{tweetId}")
+    public Tweet deleteTweet(@PathVariable Long tweetId) {
+        return tweetService.deleteTweet(tweetId);
     }
 }

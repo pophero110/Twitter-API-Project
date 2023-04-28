01e182b4026ece577a6eb676eb679a2bb5725be0
Jeff Ou <ojn2014520@gmail.com>
2023-04-28 18:52:32 -0500

    hide user data on each tweet

--- src/main/java/com/jeffdev/twitterapi/model/Tweet.java
+++ src/main/java/com/jeffdev/twitterapi/model/Tweet.java
@@ -1,5 +1,6 @@
 package com.jeffdev.twitterapi.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -31,6 +32,7 @@ public class Tweet {
 
     @ManyToOne
     @JoinColumn(name = "user_id")
+    @JsonIgnore
     private User user;
 
     public Tweet() {

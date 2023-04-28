72463c94609f4e7aeee74510032310b0fadc4346
Jeff Ou <ojn2014520@gmail.com>
2023-04-28 12:29:00 -0500

    Create tweet service
    1. add createTweet method
    2. use TweetService in TweetController

--- src/main/java/com/jeffdev/twitterapi/controller/TweetController.java
+++ src/main/java/com/jeffdev/twitterapi/controller/TweetController.java
@@ -2,6 +2,8 @@ package com.jeffdev.twitterapi.controller;
 
 
 import com.jeffdev.twitterapi.model.Tweet;
+import com.jeffdev.twitterapi.service.TweetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api/tweets")
 public class TweetController {
 
+    private TweetService tweetService;
+
+    @Autowired
+    public void setTweetService(TweetService tweetService) {
+        this.tweetService = tweetService;
+    }
 
     /**
      * Create a new tweet and return it
@@ -19,6 +27,6 @@ public class TweetController {
      */
     @PostMapping(path = "")
     public Tweet createTweet(@RequestBody Tweet tweetObject) {
-        return new Tweet();
+        return tweetService.createTweet(tweetObject);
     }
 }

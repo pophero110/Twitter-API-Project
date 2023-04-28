1302188cf917dbb99003f2a8c82159e1bec7ad1b
Jeff Ou <ojn2014520@gmail.com>
2023-04-28 11:27:59 -0500

    Add getter, setter, constructor, and toString for tweet model

--- src/main/java/com/jeffdev/twitterapi/model/Tweet.java
+++ src/main/java/com/jeffdev/twitterapi/model/Tweet.java
@@ -25,4 +25,56 @@ public class Tweet {
 
     @LastModifiedBy
     private Instant updatedAt;
+
+    public Tweet() {
+    }
+
+    public Tweet(long id, String content, Instant createdAt, Instant updatedAt) {
+        this.id = id;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Tweet{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
 }

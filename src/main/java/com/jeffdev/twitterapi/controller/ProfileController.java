a5a1ed2a45b490708b1eba491289584f31e1ef85
Jeff Ou <ojn2014520@gmail.com>
2023-04-28 21:51:19 -0500

    Create CRU endpoints for Profile

---
+++ src/main/java/com/jeffdev/twitterapi/controller/ProfileController.java
@@ -0,0 +1,52 @@
+package com.jeffdev.twitterapi.controller;
+
+import com.jeffdev.twitterapi.model.Profile;
+import com.jeffdev.twitterapi.service.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.Entity;
+
+@RestController
+@RequestMapping(path = "/api/profiles")
+public class ProfileController {
+
+    private ProfileService profileService;
+
+    @Autowired
+    public void setProfileService(ProfileService profileService) {
+        this.profileService = profileService;
+    }
+
+    /**
+     * create a profile in database and return it
+     *
+     * @param profileObject the profile object that needs to be created
+     * @return created profile
+     */
+    @PostMapping("")
+    public Profile createProfile(@RequestBody Profile profileObject) {
+        return profileService.createProfile(profileObject);
+    }
+
+    /**
+     * update a profile in database and return it
+     *
+     * @param profileObject the object that contains attributes that need be updated
+     * @return updated profile
+     */
+    @PutMapping("")
+    public Profile updateProfile(@RequestBody Profile profileObject) {
+        return profileService.updateProfile(profileObject);
+    }
+
+    /**
+     * Get a specific user's profile by user id
+     * @param userId the id of the user that profile belongs to
+     * @return Profile
+     */
+    @GetMapping("/users/{userId}")
+    public Profile getProfile(@PathVariable Long userId) {
+        return profileService.getProfileByUserId(userId);
+    }
+}
